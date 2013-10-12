9961ee668c28f82fad7b1b47c5e54709ca68737c
HarryMiller <HarryMillerMail@googlemail.com>
2013-10-12 19:55:29 +0200

    hw1 part 1 ok

--- hw0/hw0.rb
+++ hw0/hw0.rb
@@ -1,4 +1,7 @@
 
+################
+# part 1
+
 def sum(arr)
   sum = 0
   arr.each do | item | 
@@ -56,6 +59,57 @@ def b2inary_multiple_of_4?(str)
   val = str.to_i(2)
   return val  % 4 == 0
 end
+
+
+####################
+# part 2
+
+
+def sum(arr)
+  sum = 0
+  arr.each do | item |
+     sum = sum + item
+  end
+  sum
+end
+
+
+
+
+def max_2_sum(arr)
+  sum = 0
+  sorted_array = arr.sort.reverse
+  if sorted_array.length >= 2
+    sum = sorted_array[0] + sorted_array[1]
+  elsif sorted_array.length == 1
+    sum = sorted_array[0]
+  end
+  sum
+end
+
+
+
+
+def sum_to_n?(arr, n)
+  hash = {0=>true}
+  i=0
+  while i < arr.length
+    j = i+1
+    while j < arr.length
+      sum = arr[i] + arr[j]
+      hash[sum] = true
+      j = j+1
+    end
+    i = i+1
+  end
+  return hash.key?(n)
+end
+
+
+
+
+###############
+# part 3
 
 
 
--- hw1/public/lib/fun_with_strings.rb
+++ hw1/public/lib/fun_with_strings.rb
@@ -1,12 +1,45 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+		# convert all letter to downcase
+		# wipe out all not-letters
+		org = self.downcase.gsub(/\W/, '')
+    s = org.reverse
+    s == org
   end
+
   def count_words
-    # your code here
+		hash = {}
+		# replace non words with blank, so only words
+		# are in the string.
+    self.downcase.gsub(/\W/, ' ').split.each do |word |
+			if (hash[word] == nil)
+				hash[word] = 1
+			else
+				# found one more times
+				hash[word] = hash[word] + 1
+			end
+		end
+	  hash
   end
+
   def anagram_groups
-    # your code here
+		aWord = self.downcase.gsub(/\W/, ' ').split
+		hGroup = {}
+		aWord.each do | word |
+			# create an unquie key
+			# just take the string and sort it
+			aChar = []
+			word.each_char { |c|aChar.push(c) }
+			aChar.sort!
+			key = aChar.join
+			if (!hGroup.key?(key))
+				hGroup[key] = [word]
+			else
+				hGroup[key].push(word)
+			end
+		end
+		# collect all hash-values (array) into an array
+    hGroup.collect { |k,v| v }
   end
 end
 
