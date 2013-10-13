75010b5a6d6a357c5b24daa055e1951142dc60ab
HarryMiller <HarryMillerMail@googlemail.com>
2013-10-13 12:25:08 +0200

    hw1 finished

--- hw1/public/lib/attr_accessor_with_history.rb
+++ hw1/public/lib/attr_accessor_with_history.rb
@@ -1,10 +1,41 @@
 class Class
   def attr_accessor_with_history(attr_name)
     attr_name = attr_name.to_s # make sure it's a string
-    attr_reader attr_name # create the attribute's getter
-    attr_reader attr_name+"_history" # create bar_history getter
+
     class_eval %Q{
-      # YOUR CODE HERE
+
+      def #{attr_name}; 
+        @#{attr_name} = [nil] if @#{attr_name} == nil;
+        @#{attr_name}[-1]; end;
+
+      def #{attr_name}= (arg); 
+        @#{attr_name} = [nil] if @#{attr_name} == nil;
+        @#{attr_name}.push(arg); 
+        end; 
+
+      def #{attr_name+"_history"}; 
+        return nil if @#{attr_name} == nil;
+        @#{attr_name}[0..-2].collect{|c|c}; 
+        end;
     }
   end
+
+
 end
+
+
+
+
+class TestClass1
+  attr_accessor_with_history :foo
+end
+
+
+
+c = TestClass1.new
+#puts(c.foo.class)
+c.foo = "hallo"
+c.foo = 5 
+
+#puts(c.foo_history)
+
--- hw1/public/lib/rock_paper_scissors.rb
+++ hw1/public/lib/rock_paper_scissors.rb
@@ -4,11 +4,47 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    s1 = player1[1]
+    s2 = player2[1]
+    # check if there are valid strategies
+    raise NoSuchStrategyError, "Strategy must be one of R,P,S" unless s1.match(/[RPS]/i)
+    raise NoSuchStrategyError, "Strategy must be one of R,P,S" unless s2.match(/[RPS]/i)
+    # both players with same strategy
+    return player1 if s1 == s2
+    hVal = {'R' => 0, 'S' => 1, 'P' => 2}
+    val1 = hVal[s1]
+    val2 = hVal[s2]
+    diff = (val1 - val2) % 3
+    return player1 if diff.even?
+    return player2
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    t1 = tournament[0]
+    t2 = tournament[1]
+    if t1.class == Array  &&  t1[0].class == Array
+      p1 = tournament_winner(t1)
+      p2 = tournament_winner(t2)
+      return winner(p1, p2)
+    else
+      return winner(t1, t2)
+    end
   end
 
 end
+
+
+t = [
+    [
+        [ ["Armando", "P"], ["Dave", "S"] ],
+        [ ["Richard", "R"],  ["Michael", "S"] ],
+    ],
+    [
+        [ ["Allen", "S"], ["Omer", "P"] ],
+        [ ["David E.", "R"], ["Richard X.", "P"] ]
+    ]
+]
+#t = [["Armando", "P"], ["Dave", "S"]]
+#a = RockPaperScissors.tournament_winner(t)
+#print(a)
+
